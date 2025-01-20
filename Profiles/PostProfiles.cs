194e91602dcd12c93b75b1cab230452e93b53c91
MaaRadha <Omyoga2016@outlook.com>
2025-01-20 01:49:34 +0100

    added profiles for mapping

---
+++ Profiles/PostProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FeedBackWebApi.DTO.PostCommentDTOs;
+using FeedBackWebApi.Models;
+
+namespace FeedBackWebApi.Profiles
+{
+    public class PostProfiles : Profile
+    {
+        public PostProfiles()
+        {
+            CreateMap<PostComment, PostReadCommentDto>();
+            CreateMap<PostUpdateCommentDto, PostComment>();
+            CreateMap<PostCreateCommentDto, PostComment>();
+        }
+    }
+}
---
+++ Profiles/ReactionProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FeedBackWebApi.DTO.PostReactionDTOs;
+using FeedBackWebApi.Models;
+
+namespace FeedBackWebApi.Profiles
+{
+    public class ReactionProfiles : Profile
+    {
+        public ReactionProfiles()
+        {
+            CreateMap<PostReaction, PostReadReactionDto>();
+            CreateMap<PostUpdataReactionDto, PostReaction>();
+            CreateMap<PostCreateReactionDto, PostReaction>();
+
+        }
+    }
+}
